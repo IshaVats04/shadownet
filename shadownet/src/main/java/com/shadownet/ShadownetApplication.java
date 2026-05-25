e17e39be8ccd1b03f730c7970d1dc63de81489de
IshaVats04 <ishavats04@gmail.com>
2026-05-25 20:57:48 +0530

    Initial commit

---
+++ shadownet/src/main/java/com/shadownet/ShadownetApplication.java
@@ -0,0 +1,13 @@
+package com.shadownet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadownetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShadownetApplication.class, args);
+	}
+
+}
---
+++ shadownet/src/test/java/com/shadownet/ShadownetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadownet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShadownetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
